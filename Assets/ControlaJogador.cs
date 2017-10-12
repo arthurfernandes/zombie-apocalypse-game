45017b2329d240726ecff72e67725c430d02f525
Arthur Fernandes Araujo <arthurfernandes05@gmail.com>
2017-10-11 21:22:43 -0300

    Adicionando animacoes vinculadas a movimentos

--- Assets/ControlaJogador.cs
+++ Assets/ControlaJogador.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ControlaJogador : MonoBehaviour {
 
+	public float Velocidade = 10;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,17 @@ public class ControlaJogador : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward);
+		float eixoX = Input.GetAxis ("Horizontal");
+		float eixoZ = Input.GetAxis ("Vertical");
+
+		Vector3 direcao = new Vector3 (eixoX, 0, eixoZ);
+
+		transform.Translate (direcao * Velocidade * Time.deltaTime);
+
+		if (direcao != Vector3.zero) {
+			GetComponent<Animator> ().SetBool ("Movendo", true);		
+		} else {
+			GetComponent<Animator> ().SetBool ("Movendo", false);
+		}
 	}
 }
